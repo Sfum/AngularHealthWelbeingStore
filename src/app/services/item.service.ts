507cdd0545e936e9757e223f6af5648b6746016c
Plamen M <pulsedloops@gmail.com>
2024-01-04 08:32:49 +0100

    Create Header

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable, tap} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {tap} from "rxjs";
 import {Item} from "../model/item";
 
 @Injectable({
