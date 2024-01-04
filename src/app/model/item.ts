969e8852e6a67447f8fd8860b262788105bd285c
Plamen M <pulsedloops@gmail.com>
2024-01-04 10:52:20 +0100

    Create responsive Mat-card to display content
    The card leaves larger margin on larger screen and smaller margin on smaller screen sizes

--- src/app/model/item.ts
+++ src/app/model/item.ts
@@ -1,11 +1,11 @@
 export interface Item {
   id: number,
   item_name: string,
-  // item_price: number,
-  // item_price_detail: number,
-  // item_wishlist: boolean,
-  // item_description: string,
-  // item_manufacturer: string,
-  // item_expiry_date: Date,
-  // item_in_stock: boolean,
+  item_price: number,
+  item_price_detail: number,
+  item_wishlist: boolean,
+  item_description: string,
+  item_manufacturer: string,
+  item_expiry_date: Date,
+  item_in_stock: boolean,
 }
